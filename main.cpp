a1d151f61c178958d9004480904de6f696eda797
tytjan <adrianczech-1997@wp.pl>
2021-01-02 22:41:35 +0100

    producer consumer

--- main.cpp
+++ main.cpp
@@ -1,30 +1,82 @@
 #include <iostream>
 #include "Monitor.h"
 #include "Buffer.h"
+#include <vector>
 
-
-static unsigned int dumplingsCount, fillingsPerProducer, doughsMade = 0, dumplingsMade = 0;
+static unsigned int doughsMade = 0, doughsCount, fillingsPerProducer, dumplingsMade = 0;
 
 enum Type
 {
-    dough, meat, cheese, cole
+    dough, meat, cheese, cole, none
 };
 
-class Product
+std::string typeToString(Type& type)
+{
+    switch(type)
+    {
+        case dough:
+            return "dough";
+        case meat:
+            return "meat";
+        case cheese:
+            return "cheese";
+        case cole:
+            return "cole";
+        default:
+            return "";
+    }
+}
+
+struct Product
 {
     Type type;
 public:
-    Product(Type type) : type(type) {}
-    Product(Product& product) : type(product.type) {}
+    explicit Product(Type type) : type(type) {}
+    Product(Product& product) = default;
 };
+
+typedef std::vector<Buffer<Product>> bufVec;
 
 void doughProducer(Buffer <Product>& doughBuffer)
 {
-    while(dumplingsMade < dumplingsCount)
+    while(doughsMade < doughsCount)
     {
         doughBuffer.put(Product(dough));
         dumplingsMade++;
     }
+}
+
+void fillingProducer(Type type, Buffer <Product>& buffer)
+{
+    unsigned int fillingsMade = 0;
+    while(fillingsMade < fillingsPerProducer)
+    {
+        buffer.put(Product(type));
+        fillingsMade++;
+    }
+}
+
+void dumplingProducer(bufVec& buffers)
+{
+    while(dumplingsMade < doughsCount)
+    {
+        auto nextDough = buffers[dough].get();
+        bool fillingAvailable = false;
+        for(int i = 1; i < 4; i++)
+        {
+            Product nextFilling(none);
+            if(buffers[i].tryToGet(nextFilling))
+            {
+                fillingAvailable = true;
+                std::cout << "Zrobiono pieroga z " << typeToString(nextFilling.type);
+                break;
+            }
+        }//for
+        if(!fillingAvailable)
+        {
+            buffers[dough].put(nextDough);
+        }
+    }//while
 }
 
 int main(int argc, char** argv)
@@ -32,10 +84,19 @@ int main(int argc, char** argv)
     unsigned int bufferSize, fillingProducerCount;
     unsigned int doughProducerCount;
 
-    bufferSize = atoi(argv[1]);
-    doughProducerCount = atoi(argv[2]);
-    fillingProducerCount = atoi(argv[3]);
-    fillingsPerProducer = atoi(argv[4]);
+    if(argc < 5)
+    {
+        std::cin >> bufferSize >> doughProducerCount >> fillingProducerCount >> fillingsPerProducer;
+    }
+    else
+    {
+        bufferSize = atoi(argv[1]);
+        doughProducerCount = atoi(argv[2]);
+        fillingProducerCount = atoi(argv[3]);
+        fillingsPerProducer = atoi(argv[4]);
+    }
 
-    dumplingsCount = fillingsPerProducer * fillingProducerCount;
+    doughsCount = fillingsPerProducer * fillingProducerCount;
+
+
 }
